39561e58b22b69e055a9d3e0434b6b4902ecbc01
John Walker <jewalker7842@gmail.com>
2026-02-26 21:04:39 -0600

    changed retention numbers

--- infra/main.tf
+++ infra/main.tf
@@ -13,7 +13,7 @@ resource "azurerm_log_analytics_workspace" "law" {
   resource_group_name = azurerm_resource_group.rg.name
 
   sku               = "PerGB2018"
-  retention_in_days = 7
+  retention_in_days = 30
 }
 
 resource "azurerm_container_app_environment" "cae" {
